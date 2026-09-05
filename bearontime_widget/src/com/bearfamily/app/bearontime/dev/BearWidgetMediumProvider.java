654bb3c2232fd6db66c2f17b288a88769beaa153
Hsiung2004 <hsiung2004@gmail.com>
2026-09-05 17:20:59 +0800

    Add medium widget provider

---
+++ bearontime_widget/src/com/bearfamily/app/bearontime/dev/BearWidgetMediumProvider.java
@@ -0,0 +1,6 @@
+package com.bearfamily.app.bearontime.dev;
+
+public final class BearWidgetMediumProvider extends BearWidgetBaseProvider {
+    @Override protected String layoutName() { return "bear_widget_medium"; }
+    @Override protected String sizeName() { return "medium"; }
+}
