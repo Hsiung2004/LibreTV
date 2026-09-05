1fda71a20b696a07a6b4cf77be3fd346710b548b
Hsiung2004 <hsiung2004@gmail.com>
2026-09-05 17:20:51 +0800

    Add small widget provider

---
+++ bearontime_widget/src/com/bearfamily/app/bearontime/dev/BearWidgetSmallProvider.java
@@ -0,0 +1,6 @@
+package com.bearfamily.app.bearontime.dev;
+
+public final class BearWidgetSmallProvider extends BearWidgetBaseProvider {
+    @Override protected String layoutName() { return "bear_widget_small"; }
+    @Override protected String sizeName() { return "small"; }
+}
