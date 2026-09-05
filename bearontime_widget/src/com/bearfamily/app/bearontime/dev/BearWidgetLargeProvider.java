ee0f974a54168956afb37f945ba909ec5a13c5ff
Hsiung2004 <hsiung2004@gmail.com>
2026-09-05 17:21:08 +0800

    Add large widget provider

---
+++ bearontime_widget/src/com/bearfamily/app/bearontime/dev/BearWidgetLargeProvider.java
@@ -0,0 +1,6 @@
+package com.bearfamily.app.bearontime.dev;
+
+public final class BearWidgetLargeProvider extends BearWidgetBaseProvider {
+    @Override protected String layoutName() { return "bear_widget_large"; }
+    @Override protected String sizeName() { return "large"; }
+}
